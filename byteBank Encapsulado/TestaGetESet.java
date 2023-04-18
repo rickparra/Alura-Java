41a7349a657ef85967d47dbbf8fe97a4bfa9f92b
Parra <113686045+rickparra@users.noreply.github.com>
2023-04-17 23:05:35 -0300

    Add files via upload

---
+++ byteBank Encapsulado/TestaGetESet.java
@@ -0,0 +1,20 @@
+
+public class TestaGetESet {
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337,24226);
+		conta.setNumero(1337);
+
+		System.out.println(conta.getNumero());
+		
+		Cliente parra = new Cliente();
+		
+		parra.setNome("Henrique Parra");
+		
+		
+		conta.setTitular(parra);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+	}
+}
